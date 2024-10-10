1242740a55801694244cec3d6663fc357e04b842
damdaris <darisadam@upi.edu>
2024-10-11 00:00:54 +0700

    Initial Commit

---
+++ Dicee 2/Dicee_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_2App.swift
+//  Dicee 2
+//
+//  Created by Adam Daris Ryadhi on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
